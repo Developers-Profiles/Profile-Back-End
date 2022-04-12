66184dde6915d36d87e628e36af42d992eab7546
marvinjoel <marvin.roses@gmail.com>
2022-04-12 14:37:13 -0500

    FEAD: database creation

--- status/models.py
+++ status/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class TypeUser(models.TextChoices):
+    BACK_END = 'Back End'
+    FRONT_END = 'Front End'
+    FULL_STACK = 'Full Stack'
+
+
+class SocialNetworks(models.Model):
+    name = models.CharField(max_length=50)
+    link_sn = models.CharField(max_length=200)
+
+
+class StatusUserModel(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200, blank=True, null=True)
+    social_networks = models.ForeignKey(SocialNetworks, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='img')
+    type_user = models.CharField(max_length=11, choices=TypeUser.choices, default=TypeUser.FULL_STACK)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Type {self.type_user} by {self.user.id}"
+
+    class Meta:
+        verbose_name = 'StatusUserModel'
+        verbose_name_plural = 'StatusUserModels'
+        ordering = ['id']
--- user/models.py
+++ user/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from user.manager import UserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    is_email_verified = models.BooleanField(default=False)
+    email_verification_token = models.CharField(max_length=20, null=True, blank=True)
+    forget_password_token = models.CharField(max_length=200, null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username',]
+
+    objects = UserManager()
+
+    def name(self):
+        return self.first_name + ' '+ self.last_name
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['id']
